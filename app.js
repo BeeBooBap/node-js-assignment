c60e8fd6c014844ad24ffab2103c2aed9936878d
Ayesha Bhatti <ayebhatt1@WKMZTB758B72.global.publicisgroupe.net>
2022-12-19 11:01:08 +0000

    Adding routes

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const express = require('express');
 // create express app
 const app = express();
 
+// routes
+app.use('/itunes', (req, res, next) => {
+    console.log('This is where we will make an API call to the iTunes API');
+})
+
+app.use('/google', (req, res, next) => {
+    console.log('This is where we will make an API call to the Google Places API');
+})
+
+app.use('/', (req, res, next) => {
+    console.log('This is the home page');
+})
+
 // start server
 app.listen(3000, () => {
     console.log(`Listening on port ${3000}`);
