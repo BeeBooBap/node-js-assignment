cf2e497aaa1aa26c0ca772a1ba9425edc6e01bdd
Ayesha Bhatti <ayebhatt1@WKMZTB758B72.global.publicisgroupe.net>
2022-12-20 19:29:50 +0000

    Making calls to spotify API to authenticate user

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 // imports
 const express = require('express');
 
-const PORT = 8080;
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+
+const scopes = [
+    'ugc-image-upload',
+    'user-read-playback-state',
+    'user-modify-playback-state',
+    'user-read-currently-playing',
+    'streaming',
+    'app-remote-control',
+    'user-read-email',
+    'user-read-private',
+    'playlist-read-collaborative',
+    'playlist-modify-public',
+    'playlist-read-private',
+    'playlist-modify-private',
+    'user-library-modify',
+    'user-library-read',
+    'user-top-read',
+    'user-read-playback-position',
+    'user-read-recently-played',
+    'user-follow-read',
+    'user-follow-modify'
+  ];
+
+// Instantiate wrapper
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: process.env.REDIRECT_URI
+});
 
 // create express app
 const app = express();
-
-// routes
-const musicData = require('./routes/music');
-const spaceData = require('./routes/space');
 
 // middleware to parse incoming json data
 app.use(bodyParser.json()); // application/json
@@ -20,15 +49,71 @@ app.use((req, res, next) => {
     next();
 });
 
+// routes
+const musicData = require('./routes/music');
+const spaceData = require('./routes/space');
+
 app.use('/music', musicData.routes);
 app.use('/space', spaceData.routes);
 
+// authenticate user 
+app.get('/spotify-login', (req, res, next) => {
+    res.redirect(spotifyApi.createAuthorizeURL(scopes));
+});
+
+// user redirected upon good auth to /music/artist
+app.get('/callback', (req, res, next) => {
+    const error = req.query.error;
+    const code = req.query.code;
+    const state = req.query.state;
+  
+    if (error) {
+      console.error('Callback Error:', error);
+      res.send(`Callback Error: ${error}`);
+      return;
+    }
+  
+    spotifyApi
+      .authorizationCodeGrant(code)
+      .then(data => {
+        const access_token = data.body['access_token'];
+        const refresh_token = data.body['refresh_token'];
+        const expires_in = data.body['expires_in'];
+  
+        spotifyApi.setAccessToken(access_token);
+        spotifyApi.setRefreshToken(refresh_token);
+  
+        console.log('access_token:', access_token);
+        console.log('refresh_token:', refresh_token);
+  
+        console.log(
+          `Sucessfully retreived access token. Expires in ${expires_in} s.`
+        );
+
+        process.env.SPOTIFY_ACCESS_TOKEN = access_token;
+        res.redirect('/music/artist');
+  
+        setInterval(async () => {
+          const data = await spotifyApi.refreshAccessToken();
+          const access_token = data.body['access_token'];
+  
+          console.log('The access token has been refreshed!');
+          console.log('access_token:', access_token);
+          spotifyApi.setAccessToken(access_token);
+        }, expires_in / 2 * 1000);
+      })
+      .catch(error => {
+        console.error('Error getting Tokens:', error);
+        res.send(`Error getting Tokens: ${error}`);
+      });
+    });
+
 // catch all middleware
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+    res.send('This is the home page!');
 });
 
 // start server
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
 });
