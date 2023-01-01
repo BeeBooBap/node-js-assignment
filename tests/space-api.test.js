a850d48923dd7a4bf5846d3408f0a9d360b08aae
Ayesha Bhatti <ayebhatt1@WKMZTB758B72.global.publicisgroupe.net>
2023-01-01 16:47:06 +0000

    Adding tests for space api - 1 failing

---
+++ tests/space-api.test.js
@@ -0,0 +1,35 @@
+const request = require("supertest")
+const baseURL = "http://localhost:8080"
+
+describe("GET /nasa-api/v1/apod", () => {
+    it("should return 200", async () => {
+        const response = await request(baseURL).get("/nasa-api/v1/apod");
+        expect(response.statusCode).toBe(200);
+    });
+    it("should return the title of the image of the day", async () => {
+        const response = await request(baseURL).get("/nasa-api/v1/apod");
+        expect(response.body.title === 'The Largest Rock in our Solar System').toBe(true);
+    });
+});
+
+describe("GET /nasa-api/v1/mars-rover", () => {
+    it("should return 200", async () => {
+        const response = await request(baseURL).get("/nasa-api/v1/mars-rover");
+        expect(response.statusCode).toBe(200);
+    });
+    it("should return the id of the mars rover data", async () => {
+        const response = await request(baseURL).get("/nasa-api/v1/mars-rover");
+        expect(response.body.length >= 1).toBe(true);
+    });
+});
+
+describe("GET /nasa-api/v1/cme-analysis", () => {
+    it("should return 200", async () => {
+        const response = await request(baseURL).get("/nasa-api/v1/cme-analysis");
+        expect(response.statusCode).toBe(200);
+    });
+    it("should return cme analysis", async () => {
+        const response = await request(baseURL).get("/nasa-api/v1/cme-analysis");
+        expect(response.body.length >= 1).toBe(true);
+    });
+});
